d01b097d2bb4201117db7c9d6e6cd0a54a254978
Brian Firestone <bfirestone2339@gmail.com>
2021-02-23 15:22:06 -0500

    Done.

---
+++ db/migrate/20210223182152_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[5.0]
+  def change
+    create_table :reservations do |t|
+      t.date :checkin
+      t.date :checkout
+      t.belongs_to :listing
+      t.belongs_to :guest
+
+      t.timestamps
+    end
+  end
+end
